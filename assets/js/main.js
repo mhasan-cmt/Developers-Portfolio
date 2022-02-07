b23dbe452285763faf714c388955190745aba5ef
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-02-07 22:17:13 +0600

    Qualification Tab Functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,25 @@ skillsHeader.forEach((el)=>{
 });
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach((tab)=>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active');
+        });
+
+        target.classList.add('qualification_active');
+
+        tabs.forEach(tab=>{
+            tab.classList.remove('qualification_active')
+        });
+        tab.classList.add('qualification_active')
+    });
+});
 
 /*==================== SERVICES MODAL ====================*/
 

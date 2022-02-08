b61fd95f30e72aa941addff7e84fb06dbf710699
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-02-08 18:27:57 +0600

    back to top design and functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,6 +152,14 @@ function scrollHeader(){
 window.addEventListener('scroll', scrollHeader);
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560){
+        scrollUp.classList.add('show-scroll');
+    }else{
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollUp);
 
 /*==================== DARK LIGHT THEME ====================*/ 

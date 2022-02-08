8189960ad2c439fa48e178ae86ee481ec40a55dd
Mahmudul Hasan Shafin <mhshafin220@gmail.com>
2022-02-08 12:48:55 +0600

    swiper functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -89,7 +89,18 @@ modalCloses.forEach((modalClose)=>{
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".portfolio_container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
